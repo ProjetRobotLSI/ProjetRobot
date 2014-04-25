0e480a5a7bdeb2251a1cf0d3aad53e3124fbb3a1
karimkane <karim.kane19@gmail.com>
2014-04-25 11:30:18 +0200

    Rajout video-conf

--- KercarAndroid/src/com/kercar/AsyncTask/ThreadPing.java
+++ KercarAndroid/src/com/kercar/AsyncTask/ThreadPing.java
@@ -20,7 +20,7 @@ public class ThreadPing extends Thread{
 		this.context = context;
 		this.activity = activity;
 	}
-	
+/*	
 	@Override
 	public void run() {
 		while(true){
@@ -43,4 +43,5 @@ public class ThreadPing extends Thread{
 			}
 		}
 	}
+*/
 }
